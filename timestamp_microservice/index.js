46539a434793c987b4a49daeee74a36c3b98b927
Bilou <thomas.bardon@outlook.com>
2022-12-27 03:34:38 +0100

    first commit

---
+++ timestamp_microservice/index.js
@@ -0,0 +1,37 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/:date?", function (req, res) {
+  let unix = Number(req.params.date);
+  if (isNaN(unix)) res.json({ error: 'Invalid Date' });
+  if (!unix) unix = Date.now();
+
+  const utc = new Date(unix).toUTCString();
+  res.json({
+    unix: unix,
+    utc: utc
+  });
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
